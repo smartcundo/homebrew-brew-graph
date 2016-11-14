c31e7395a75fd9cd1cec125bbba0d808c27cd3c5
cundo <cundo@hotmail.com>
2016-11-14 11:37:38 -0800

    brew tap created

---
+++ Formula/smartiamcreator.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class BrewDesc < Formula
+  homepage 'https://github.com/smartcundo/smartiamcreator'
+  url 'https://github.com/smartcundo/homebrew-smartiamcreator.git'
+  version '0.0.1'
+
+  def install
+    opoo <<-EOS.undent
+      Once you've tapped this repository, you've got the command. 
+      And whenever you run `brew update`, this command will be updated automatically.
+    EOS
+  end
+end
