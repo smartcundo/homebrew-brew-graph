e0b2fb21fa682c63b5101ca660ca3e96fb1705db
cundo <cundo@hotmail.com>
2016-11-15 11:00:11 -0800

    Adding python support to install script

--- Formula/smartiamcreator.rb
+++ Formula/smartiamcreator.rb
@@ -6,10 +6,28 @@ class Smartiamcreator < Formula
   version '0.0.1'
 
   def install
-    opoo <<-EOS.undent
-      Once you've tapped this repository, you've got the command. 
-      And whenever you run `brew update`, this command will be updated automatically.
-    EOS
+    Language::Python.each_python(build) do |python, version|
+      ENV.prepend_create_path "PATH", buildpath/"vendor/bin"
+      ENV.prepend_create_path "PYTHONPATH", buildpath/"vendor/lib/python#{version}/site-packages"
+      resource("cython").stage do
+        system python, *Language::Python.setup_install_args(buildpath/"vendor")
+      end
+
+      bundle_path = libexec/"lib/python#{version}/site-packages"
+      ENV.prepend_create_path "PYTHONPATH", bundle_path
+      resource("six").stage do
+        system python, *Language::Python.setup_install_args(libexec)
+      end
+      (lib/"python#{version}/site-packages/homebrew-h5py-bundle.pth").write "#{bundle_path}\n"
+
+      args = Language::Python.setup_install_args(prefix)
+      args << "configure"
+      args << "--hdf5=#{Formula["homebrew/science/hdf5"].opt_prefix}"
+      args << "--mpi" if build.with? :mpi
+
+      ENV.prepend_create_path "PYTHONPATH", lib/"python#{version}/site-packages"
+      system python, *args
+    end
   end
 end
 
