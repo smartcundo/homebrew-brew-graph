9e690e0daa3ac863ae4f7cc98f1d6f25a1a2b78e
cundo <cundo@hotmail.com>
2016-11-16 16:36:23 -0800

    Adding debug puts line to ensure install is complete

--- Formula/smartiamcreator.rb
+++ Formula/smartiamcreator.rb
@@ -5,7 +5,20 @@ class Smartiamcreator < Formula
   url 'https://github.com/smartcundo/smartiamcreator/archive/0.0.2.tar.gz'
   sha256 '8b11e2384cfc11d388eb6681420ac6f830b85571972c4f42fe01c9588d30ef9f'
 
+  resource "boto" do
+    url "https://pypi.python.org/packages/source/b/boto/boto-2.36.0.tar.gz"
+    sha256 "8033c6f7a7252976df0137b62536cfe38f1dbd1ef443a7a6d8bc06c063bc36bd"
+  end
+
   def install
+    boto_path = libexec/"boto/lib/python2.7/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", boto_path
+    resource("boto").stage do
+      system "python", *Language::Python.setup_install_args(libexec/"boto")
+    end
+    (lib/"python2.7/site-packages").mkpath
+    (lib/"python2.7/site-packages/homebrew-smart-boto.pth").write "#{boto_path}\n"
+
     Language::Python.each_python(build) do |python, version|
       ENV.prepend_create_path "PATH", buildpath/"vendor/bin"
       ENV.prepend_create_path "PYTHONPATH", buildpath/"vendor/lib/python#{version}/site-packages"
